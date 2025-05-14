9b3d9c78eb5b2f0362ed014705ff70913ea1fbbd
Kiran Kumar V <imkirankumar23@gmail.com>
2025-05-14 12:51:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import streamlit as st
 import webbrowser
 import os
+
+# Your Streamlit app content
+st.title("Birthday Cake Page")
 
 # Path to the HTML file
 html_file = 'birthday-cake.html'
 
 # Check if the file exists
 if os.path.exists(html_file):
-    # Open the HTML file in the default web browser
-    webbrowser.open('file://' + os.path.abspath(html_file))
+    # Button to open the HTML file in the browser
+    if st.button('Open Birthday Cake Page'):
+        webbrowser.open('file://' + os.path.abspath(html_file))
 else:
-    print(f"File '{html_file}' not found.")
+    st.error(f"File '{html_file}' not found.")
