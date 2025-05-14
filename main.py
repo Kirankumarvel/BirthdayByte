acff0dd4a3382200efcaef16b44aa3a436896723
Kiran Kumar V <imkirankumar23@gmail.com>
2025-05-14 12:18:36 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import webbrowser
+import os
+
+# Path to the HTML file
+html_file = 'https://github.com/Kirankumarvel/BirthdayByte/blob/main/birthday-cake.html'
+
+# Check if the file exists
+if os.path.exists(html_file):
+    # Open the HTML file in the default web browser
+    webbrowser.open('file://' + os.path.abspath(html_file))
+else:
+    print(f"File '{html_file}' not found.")
